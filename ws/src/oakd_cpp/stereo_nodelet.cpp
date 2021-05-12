cea510303fec9d82b1f68e776a962cecbf068154
Brian Erickson <berickson@gmail.com>
2021-05-12 06:18:48 +0000

    cleaned up some errors, don't load robot model

--- ws/src/oakd_cpp/stereo_nodelet.cpp
+++ ws/src/oakd_cpp/stereo_nodelet.cpp
@@ -9,40 +9,36 @@
 #include "stereo_pipeline.hpp"
 #include <functional>
 
-// #include <depthai_examples/daiUtility.hpp>
-// Inludes common necessary includes for development using depthai library
 #include "depthai/depthai.hpp"
 
 #include <depthai_bridge/BridgePublisher.hpp>
 #include <depthai_bridge/ImageConverter.hpp>
 
-namespace oakd_cpp{
+namespace oakd_cpp {
 
-
- class StereoNodelet : public nodelet::Nodelet
+class StereoNodelet : public nodelet::Nodelet
 {
     public:
         virtual void onInit(){
 
-            ros::NodeHandle pnh("~");
-            ROS_ERROR("***** StereoNodelet::onInit");
+            ros::NodeHandle node_handle("~");
+            ROS_INFO("starting oakd_cpp::StereoNodelet");
             
             std::string deviceName;
             std::string camera_param_uri;
             int bad_params = 0;
             int queue_length = 1;
 
-            if(!pnh.getParam("camera_name", deviceName))
+            if(!node_handle.getParam("camera_name", deviceName))
             {
                 ROS_ERROR("Couldn't getParam camera_name");
                 throw std::runtime_error("Couldn't getParam camera_name");
             }
-            if(!pnh.getParam("camera_param_uri", camera_param_uri))
+            if(!node_handle.getParam("camera_param_uri", camera_param_uri))
             {
                 ROS_ERROR("Couldn't getParam camera_param_uri");
                 throw std::runtime_error("Couldn't getParam camara_param_uri");
             }
-
 
             StereoPipeline stereo_pipeline;
             stereo_pipeline.initDepthaiDev();
@@ -53,55 +49,53 @@ namespace oakd_cpp{
             
             // this part would be removed once we have calibration-api
             std::string left_uri = camera_param_uri +"/" + "left.yaml";
-            ROS_WARN_STREAM("loading " << left_uri);
-        
             std::string right_uri = camera_param_uri + "/" + "right.yaml";
-            
             std::string stereo_uri = camera_param_uri + "/" + "right.yaml";
-            
-
         
-            dai::rosBridge::ImageConverter converter(deviceName + "_left_camera_optical_frame", true);
-            dai::rosBridge::BridgePublisher<sensor_msgs::Image, dai::ImgFrame> leftPublish(imageDataQueues[0],
-                                                                                            pnh, 
-                                                                                            std::string("left/image"),
-                                                                                            std::bind(&dai::rosBridge::ImageConverter::toRosMsg, 
-                                                                                            &converter, 
-                                                                                            std::placeholders::_1, 
-                                                                                            std::placeholders::_2) , 
-                                                                                            queue_length,
-                                                                                            left_uri,
-                                                                                            "left");
+            // dai::rosBridge::ImageConverter converter(deviceName + "_left_camera_optical_frame", true);
+            dai::rosBridge::ImageConverter leftConverter("camera", true);
 
-            // bridgePublish.startPublisherThread();
+            dai::rosBridge::BridgePublisher<sensor_msgs::Image, dai::ImgFrame> leftPublish(
+                imageDataQueues[0],
+                node_handle, 
+                std::string("left/image"),
+                std::bind(&dai::rosBridge::ImageConverter::toRosMsg, 
+                    &leftConverter, 
+                    std::placeholders::_1, 
+                    std::placeholders::_2), 
+                queue_length,
+                left_uri,
+                "left");
+
             leftPublish.addPubisherCallback();
 
-            dai::rosBridge::ImageConverter rightconverter(deviceName + "_right_camera_optical_frame", true);
-            dai::rosBridge::BridgePublisher<sensor_msgs::Image, dai::ImgFrame> rightPublish(imageDataQueues[1],
-                                                                                            pnh, 
-                                                                                            std::string("right/image"),
-                                                                                            std::bind(&dai::rosBridge::ImageConverter::toRosMsg, 
-                                                                                            &rightconverter, 
-                                                                                            std::placeholders::_1, 
-                                                                                            std::placeholders::_2) , 
-                                                                                            queue_length,
-                                                                                            right_uri,
-                                                                                            "right");
+            dai::rosBridge::ImageConverter rightconverter("camera", true);
+            dai::rosBridge::BridgePublisher<sensor_msgs::Image, dai::ImgFrame> rightPublish(
+                imageDataQueues[1],
+                node_handle, 
+                std::string("right/image"),
+                std::bind(&dai::rosBridge::ImageConverter::toRosMsg, 
+                    &rightconverter, 
+                    std::placeholders::_1, 
+                    std::placeholders::_2), 
+                queue_length,
+                right_uri,
+                "right");
 
             rightPublish.addPubisherCallback();
 
-            // dai::rosBridge::ImageConverter depthConverter(deviceName + "_right_camera_optical_frame");
-            dai::rosBridge::BridgePublisher<sensor_msgs::Image, dai::ImgFrame> depthPublish(imageDataQueues[2],
-                                                                                            pnh, 
-                                                                                            std::string("stereo/depth"),
-                                                                                            std::bind(&dai::rosBridge::ImageConverter::toRosMsg, 
-                                                                                            &rightconverter, // since the converter has the same frame name
-                                                                                                            // and image type is also same we can reuse it
-                                                                                            std::placeholders::_1, 
-                                                                                            std::placeholders::_2) , 
-                                                                                            queue_length,
-                                                                                            stereo_uri,
-                                                                                            "stereo");
+            dai::rosBridge::BridgePublisher<sensor_msgs::Image, dai::ImgFrame> depthPublish(
+                imageDataQueues[2],
+                node_handle, 
+                std::string("depth/image"),
+                std::bind(&dai::rosBridge::ImageConverter::toRosMsg, 
+                    &rightconverter, // since the converter has the same frame name
+                                    // and image type is also same we can reuse it
+                    std::placeholders::_1, 
+                    std::placeholders::_2) , 
+                queue_length,
+                stereo_uri,
+                "depth");
 
             depthPublish.addPubisherCallback();
 
@@ -116,9 +110,8 @@ namespace oakd_cpp{
 
 PLUGINLIB_EXPORT_CLASS(oakd_cpp::StereoNodelet, nodelet::Nodelet)
 
+}   // namespace oakd_cpp
 
-}   // namespace depthai_examples
-// PLUGINLIB_EXPORT_CLASS(depthai_examples::StereoNodelet, nodelet::Nodelet)
 
 
 
