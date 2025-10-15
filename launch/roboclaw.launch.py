ccae53a0ad555762d7019522f9ff8d9e76723f2a
Brian Erickson <berickson@gmail.com>
2025-10-15 23:09:47 +0000

    realistic current limits

--- launch/roboclaw.launch.py
+++ launch/roboclaw.launch.py
@@ -49,8 +49,8 @@ def generate_launch_description():
                 'm2_qpps': 2437,
                 
                 # Current limits
-                'm1_max_current': 2.0,
-                'm2_max_current': 2.0,
+                'm1_max_current': 10.0,
+                'm2_max_current': 10.0,
                 
                 # Velocity limits
                 'max_angular_velocity': 2.0,
